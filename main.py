38549da78cc65046ec335e0a05fb1029f734b85a
Antonioneto0306 <carneiroantonio3008@gmail.com>
2025-05-30 18:57:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
         usuario = 'root',
-        senha = '3008',
+        senha = '',
         servidor = 'localhost',
-        database = 'prj_work'
+        database = ''
      )
 
 # Aqui inicializando o SQLAlchemy com a aplicação Flask
